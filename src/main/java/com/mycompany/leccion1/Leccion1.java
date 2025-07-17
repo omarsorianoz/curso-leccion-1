6ddf7a5586b71c5e372cf858b0b311c3e955d003
Omar Soriano Zárate <37226934+omarsorianoz@users.noreply.github.com>
2025-07-16 22:29:25 -0600

    Update Leccion1.java

--- src/main/java/com/mycompany/leccion1/Leccion1.java
+++ src/main/java/com/mycompany/leccion1/Leccion1.java
@@ -11,6 +11,6 @@ package com.mycompany.leccion1;
 public class Leccion1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Galaxy!");
     }
 }
