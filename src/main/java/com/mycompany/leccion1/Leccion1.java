5d76576f2bb6d6eabaf8669fde7d8d2a42d320fe
DanielTS5 <danielt.soriano@gmail.com>
2025-07-16 22:27:29 -0600

    demo1

---
+++ src/main/java/com/mycompany/leccion1/Leccion1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.leccion1;
+
+/**
+ *
+ * @author danie
+ */
+public class Leccion1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
